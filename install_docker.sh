49fcca0494eda8221c6c806d2c37d6335a8a5b45
root <root@DESKTOP-CGESNGL.localdomain>
2021-10-24 14:33:24 +0200

    Update the install_docker.sh instructions

--- install_docker.sh
+++ install_docker.sh
@@ -5,20 +5,21 @@ set -e
 
 apt-get update -y
 apt-get install -y \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 service docker start
 docker run hello-world
+
+
+
